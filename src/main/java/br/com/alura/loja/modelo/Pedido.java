21cb7fce189ed476f5d8c7aef3e37b0894e6de7e
Eduardo Quinalha <eduardoquinalha@gmail.com>
2022-07-18 19:58:51 -0300

    Adicionado classes Cliente e Pedido

---
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,56 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private BigDecimal valorTotal;
+	private LocalDate dataCadastro = LocalDate.now();
+	
+	@ManyToOne
+	private Cliente cliente;
+
+	public Pedido(BigDecimal valorTotal, Cliente cliente) {
+		super();
+		this.valorTotal = valorTotal;
+		this.cliente = cliente;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+	
+}
